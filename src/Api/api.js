f52edabd72f6e3583fa4b40b71a405f307a7a9dd
Selvamani K <130025654+selvakisar@users.noreply.github.com>
2023-11-14 13:08:24 +0530

    Update api.js

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,3 +1,3 @@
-export const APIauth="https://zonezero.onrender.com/api/user"
-export const APIfood="https://zonezero.onrender.com/api/foods"
-export const APIplan="https://zonezero.onrender.com/api/plan"
+export const APIauth="https://forget-is-good.onrender.com/api/user"
+export const APIfood="https://forget-is-good.onrender.com/api/foods"
+export const APIplan="https://forget-is-good.onrender.com/api/plan"
