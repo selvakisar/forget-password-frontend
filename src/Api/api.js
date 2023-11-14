b587b0f8c588eececedb38e2e19c9721e13e4647
selvakisar <130025654+selvakisar@users.noreply.github.com>
2023-11-14 13:23:58 +0530

    bug fix

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,7 +1,2 @@
 export const APIauth="https://forget-is-good.onrender.com/api/user"
 export const APIfood="https://forget-is-good.onrender.com/api/foods"
-<<<<<<< HEAD
-export const APIplan="https://forget-is-good.onrender.com/api/plan"
-=======
-export const APIplan="https://forget-is-good.onrender.com/api/plan"
->>>>>>> f52edabd72f6e3583fa4b40b71a405f307a7a9dd
